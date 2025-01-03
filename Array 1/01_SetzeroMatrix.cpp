99ce243f5f2b7b3782c811404445436747bf2b42
IshuSahu <ishusahu593@gmail.com>
2025-01-03 22:05:55 +0530

    Day 2 revision

--- Array 1/01_SetzeroMatrix.cpp
+++ Array 1/01_SetzeroMatrix.cpp
@@ -24,41 +24,40 @@ void Markcol(vector<vector<int>> &Matrix, int n, int m, int j)
 }
 vector<vector<int>> zeroMatrix(vector<vector<int>> &Matrix, int n, int m)
 {
-    //Brute
-    // for (int i = 0; i < n; i++)
-    // {
-    //     for (int j = 0; j < m; j++)
-    //     {
-    //         if (Matrix[i][j] == 0)
-    //         {
-    //             MarkRow(Matrix, n, m, i);
-    //             Markcol(Matrix, n, m, j);
-    //         }
-    //     }
-    // }
-    // for (int i = 0; i < n; i++)
-    // {
-    //     for (int j = 0; j < m; j++)
-    //     {
-    //         if (Matrix[i][j] == -1)
-    //         {
-    //             Matrix[i][j] = 0;
-    //         }
-    //     }
-    // }
-
+    // Brute
+    //  for (int i = 0; i < n; i++)
+    //  {
+    //      for (int j = 0; j < m; j++)
+    //      {
+    //          if (Matrix[i][j] == 0)
+    //          {
+    //              MarkRow(Matrix, n, m, i);
+    //              Markcol(Matrix, n, m, j);
+    //          }
+    //      }
+    //  }
+    //  for (int i = 0; i < n; i++)
+    //  {
+    //      for (int j = 0; j < m; j++)
+    //      {
+    //          if (Matrix[i][j] == -1)
+    //          {
+    //              Matrix[i][j] = 0;
+    //          }
+    //      }
+    //  }
 
     // Optimal
-    int row[n] ={0};
-    int col[m] ={0};
+    int row[n] = {0};
+    int col[m] = {0};
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
             if (Matrix[i][j] == 0)
             {
-               row[i]=1;
-               col[j]=1;
+                row[i] = 1;
+                col[j] = 1;
             }
         }
     }
@@ -66,7 +65,8 @@ vector<vector<int>> zeroMatrix(vector<vector<int>> &Matrix, int n, int m)
     {
         for (int j = 0; j < m; j++)
         {
-            if(row[i] || col[j]){
+            if (row[i] || col[j])
+            {
                 Matrix[i][j] = 0;
             }
         }
@@ -76,10 +76,9 @@ vector<vector<int>> zeroMatrix(vector<vector<int>> &Matrix, int n, int m)
 int main()
 {
     vector<vector<int>> matrix = {{1, 1, 1, 1}, {1, 0, 1, 0}, {1, 1, 1, 1}};
-    int n = matrix.size();   
+    int n = matrix.size();
     int m = matrix[0].size();
     vector<vector<int>> ans = zeroMatrix(matrix, n, m);
-
     cout << "The Final matrix is: n \n";
     for (auto it : ans)
     {
